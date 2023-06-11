81058228600272760d5865a27b8a7e0337a3e983
bomibonnie <bomi.may.lee@gmail.com>
2023-06-11 17:14:14 -0500

    two mode added

--- 2023_Network_Analysis_usingR.R
+++ 2023_Network_Analysis_usingR.R
@@ -9,6 +9,7 @@
 #         nodeList.csv                                           #
 #         pndata.xlsx                                            #
 #         rivdefme1980.csv                                       #
+#         twomodeedge.csv                                        #
 #  Thanks to Dr.Elizabeth Menninga for some of this code.        #
 ##################################################################
 
@@ -138,6 +139,33 @@ plot(net_igraph2,
      vertex.label.cex=1.5,
      vertex.label.dist=1.5)
 
+## Two-mode networks
+### Load the data
+edge <- read.csv("twomodeedge.csv", row.names = 1)
+
+### Change to a matrix
+edge2 <- as.matrix(edge)
+
+### to an igraph object
+net2 <- graph_from_incidence_matrix(edge2)
+net2.bp <- bipartite.projection(net2) ## make it two-mode
+
+### Make the "events" distict from actors
+V(net2)$type ## Events and actors are distinquished in type
+
+V(net2)$shape <- c("square", "circle")[V(net2)$type+1]
+V(net2)$color <- c("gray", "white")[V(net2)$type+1]
+V(net2)$lcolor <- c("navy", "black")[V(net2)$type+1]
+
+V(net2)$size <- c(1, 0.9)[V(net2)$type+1]
+V(net2)$vsize <- c(8, 5)[V(net2)$type+1]
+
+### Plot it
+windows()
+plot(net2, vertex.label.color=V(net2)$lcolor, 
+     vertex.label.cex=V(net2)$size,
+     vertex.size=V(net2)$vsize,
+     vertex.frame.color="white") 
 
 ## Real data: defense pacts (ATOP: Leeds et al. 2002)
 atop <- import("atop_sample2.csv")
